145c3825a613e5d6cab72fe1cfdc3febda04876d
Jakub Szwachła <meszkax@gmail.com>
2014-01-12 15:19:15 +0100

    Add game states

--- bomber/Game1.cs
+++ bomber/Game1.cs
@@ -24,6 +24,7 @@ namespace bomber
         public const int Height = 240;
 
         public static Random random;
+        public static BitmapFont Font;
 
         public static int WrappedY(int y)
         {
@@ -51,27 +52,16 @@ namespace bomber
             }
         }
     }
-
+     
 	/// <summary>
     /// This is the main type for your game
     /// </summary>
     public class Game1 : Game
     {
         public const int Scale = 2;
-
         private GraphicsDeviceManager graphics;
-        private Player player;
-        private Player player2;
+        private GameState currentState;
         //private Boolean resized = false;
-
-        private List<Player> playerList = new List<Player>();
-        private int[] score = new int[4] {0, 0, 0, 0};
-        private bool scored = false;
-        private int scoreDelay = 0;
-        private Player winner = null;
-        private int pointsToWin = 5;
-
-        private BitmapFont font;
 
         public Game1()
         {
@@ -94,6 +84,8 @@ namespace bomber
         {
             Globals.random = new Random();
             base.Initialize();
+            currentState = new MainGame();
+            currentState.Initialize();
         }
 
         /// <summary>
@@ -102,50 +94,8 @@ namespace bomber
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             Globals.Batch = new SpriteBatch(GraphicsDevice);
-
-            font = new BitmapFont(Content.Load<Texture2D>("Textures/c64_font.png"), 8, 10);
-
-            Dictionary<string, Keys> playerControls = new Dictionary<string, Keys> {
-                {"left", Keys.Left},
-                {"right", Keys.Right},
-                {"jump", Keys.Up},
-                {"bomb", Keys.Down},
-            };
-
-            player = new Player(0, Content.Load<Texture2D>("Textures/player_small.png"), new Rectangle(100, 5, 13, 13), playerControls, "red", Color.Red, Color.PeachPuff);
-            playerList.Add(player);
-
-            Dictionary<string, Keys> player2Controls = new Dictionary<string, Keys> {
-                {"left", Keys.A},
-                {"right", Keys.D},
-                {"jump", Keys.W},
-                {"bomb", Keys.S},
-            };
-
-            player2 = new Player(1, Content.Load<Texture2D>("Textures/player_small.png"), new Rectangle(200, 5, 13, 13), player2Controls, "blue", Color.Blue, Color.LightBlue);
-            playerList.Add(player2);
-
-            Globals.Map = new TileMap(20, 15);
-
-            Globals.Map.LoadMap(new int[,] {
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2},
-                {0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 1},
-                {1, 2, 1, 0, 1, 0, 0, 0, 0, 2, 0, 2, 2, 0, 2, 2, 2, 0, 2, 1},
-                {1, 2, 0, 1, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 1},
-                {1, 1, 0, 0, 0, 1, 0, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 1},
-                {1, 0, 0, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},
-            });
+            Globals.Font = new BitmapFont(Content.Load<Texture2D>("Textures/c64_font.png"), 8, 10);
 
         }
 
@@ -168,38 +118,8 @@ namespace bomber
             }
             */
 
-            if (scored)
-            {
-                Globals.HandleTimer(ref scoreDelay, gameTime, Restart);
-            }
-
-            Sprite.UpdateAll(gameTime);
-            Globals.Map.Update();
-            Explosion.CleanUp();
-            Bomb.CleanUp();
-            Powerup.CleanUp();
-
-            playerList.RemoveAll(p => p.Dead);
-            if (playerList.Count == 1 && !scored)
-            {
-                winner = playerList[0];
-                score[winner.Id] += 1;
-                scoreDelay = 2000;
-                scored = true;
-            }
-
-            // TODO: Add your update logic here			
+            currentState.Update(gameTime);	
             base.Update(gameTime);
-        }
-
-        private void Restart()
-        {
-            winner = null;
-            scored = false;
-            Sprite.SpriteList.Clear();
-            Explosion.ExplosionList.Clear();
-            playerList.Clear();
-            LoadContent();
         }
 
         /// <summary>
@@ -209,21 +129,9 @@ namespace bomber
         protected override void Draw(GameTime gameTime)
         {
             graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
-
             base.Draw(gameTime);
-
             Globals.Batch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, Matrix.CreateScale(Scale));
-            Sprite.DrawAll();
-            //Globals.Map.Draw();
-
-            font.DrawString(score[0].ToString(), 20, 10, Color.Red, 2);
-            font.DrawString(score[1].ToString(), 280, 10, Color.Blue, 2);
-
-            if (scored)
-            {
-                font.DrawString(string.Format("{0} player scores!", winner.ColorName), 10, 200, winner.TextColor, 2);
-            }
-
+            currentState.Draw();
             Globals.Batch.End();
         }
     }
--- bomber/GameState.cs
+++ bomber/GameState.cs
@@ -1,11 +1,127 @@
+#region Using Statements
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+#endregion
 
 namespace bomber
 {
-    public class GameState
+    public interface GameState
     {
-        public GameState()
+        void Initialize();
+        void Update(GameTime gameTime);
+        void Draw();
+    }
+
+    public class MainGame : GameState
+    {
+        private Player player;
+        private Player player2;
+        private List<Player> playerList = new List<Player>();
+        private int[] score = new int[4] {0, 0, 0, 0};
+        private bool scored = false;
+        private int scoreDelay = 0;
+        private Player winner = null;
+        private int pointsToWin = 5;
+
+        public MainGame()
         {
+        }
+
+        public void Initialize()
+        {
+            Dictionary<string, Keys> playerControls = new Dictionary<string, Keys> {
+                {"left", Keys.Left},
+                {"right", Keys.Right},
+                {"jump", Keys.Up},
+                {"bomb", Keys.Down},
+            };
+
+            player = new Player(0, Globals.Content.Load<Texture2D>("Textures/player_small.png"), new Rectangle(100, 5, 13, 13), playerControls, "red", Color.Red, Color.PeachPuff);
+            playerList.Add(player);
+
+            Dictionary<string, Keys> player2Controls = new Dictionary<string, Keys> {
+                {"left", Keys.A},
+                {"right", Keys.D},
+                {"jump", Keys.W},
+                {"bomb", Keys.S},
+            };
+
+            player2 = new Player(1, Globals.Content.Load<Texture2D>("Textures/player_small.png"), new Rectangle(200, 5, 13, 13), player2Controls, "blue", Color.Blue, Color.LightBlue);
+            playerList.Add(player2);
+
+            Globals.Map = new TileMap(20, 15);
+
+            Globals.Map.LoadMap(new int[,] {
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2},
+                {0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 1},
+                {1, 2, 1, 0, 1, 0, 0, 0, 0, 2, 0, 2, 2, 0, 2, 2, 2, 0, 2, 1},
+                {1, 2, 0, 1, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 1},
+                {1, 1, 0, 0, 0, 1, 0, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 1},
+                {1, 0, 0, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+            });
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (scored)
+            {
+                Globals.HandleTimer(ref scoreDelay, gameTime, Restart);
+            }
+
+            Sprite.UpdateAll(gameTime);
+            Globals.Map.Update();
+            Explosion.CleanUp();
+            Bomb.CleanUp();
+            Powerup.CleanUp();
+
+            playerList.RemoveAll(p => p.Dead);
+            if (playerList.Count == 1 && !scored)
+            {
+                winner = playerList[0];
+                score[winner.Id] += 1;
+                scoreDelay = 2000;
+                scored = true;
+            }
+        }
+
+        private void Restart()
+        {
+            winner = null;
+            scored = false;
+            Sprite.SpriteList.Clear();
+            Explosion.ExplosionList.Clear();
+            playerList.Clear();
+            Initialize();
+        }
+
+        public void Draw()
+        {
+            Sprite.DrawAll();
+
+            Globals.Font.DrawString(score[0].ToString(), 20, 10, Color.Red, 2);
+            Globals.Font.DrawString(score[1].ToString(), 280, 10, Color.Blue, 2);
+
+            if (scored)
+            {
+                Globals.Font.DrawString(string.Format("{0} player scores!", winner.ColorName), 10, 200, winner.TextColor, 2);
+            }
         }
     }
 }
